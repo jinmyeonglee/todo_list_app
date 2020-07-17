1805844354c987195d59748a71285d097f11caa6
jinmyeong.lee <jinmyeong.lee@linecorp.com>
2020-07-17 14:34:57 +0900

    Add stream logger for flask log

--- src/utillity.py
+++ src/utillity.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
-from logging import Formatter, DEBUG
+from logging import Formatter, DEBUG, StreamHandler
 
 
 def timestamp():
@@ -23,12 +23,20 @@ def read_sql_file(filename):
 
 
 def init_log(app):
+    formatter = Formatter(
+        '%(asctime)s %(levelname)s: %(message)s in %(filename)s:%(lineno)d]')
+
     file_handler = RotatingFileHandler("./sample-app.log",
                                        maxBytes=100000,
                                        backupCount=1000)
-
-    file_handler.setFormatter(Formatter(
-        '%(asctime)s %(levelname)s: %(message)s in %(filename)s:%(lineno)d]'))
+    file_handler.setFormatter(formatter)
     file_handler.setLevel(DEBUG)
+
+    stream_handler = StreamHandler()
+    stream_handler.setLevel(DEBUG)
+    stream_handler.setFormatter(formatter)
+
     app.logger.addHandler(file_handler)
+    app.logger.addHandler(stream_handler)
+
     app.logger.info("logging start")
