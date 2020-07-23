387e4e4f9183a583d1827b55ec1e3b69fb615322
jinmyeong.lee <jinmyeong.lee@linecorp.com>
2020-07-23 16:56:26 +0900

    Update test

--- test/test_app.py
+++ test/test_app.py
@@ -1,7 +1,6 @@
 from src.mysql import MySQLClient
-# from src.utillity import Config
+from src.utillity import Config
 
-def test_mysql():
-    db_server = MySQLClient()
-    todo_list = db_server.get_all_todo_list_with_cache()
-    assert len(todo_list) != 0
+
+def test_config():
+    assert Config().get_config()["MySQL"]["name"] == "todo_list_db"
