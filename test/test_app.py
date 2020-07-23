e54b64c4930e9037014596d6c0610930d345199b
jinmyeong.lee <jinmyeong.lee@linecorp.com>
2020-07-23 16:51:57 +0900

    Check pytest import file path

--- test/test_app.py
+++ test/test_app.py
@@ -3,5 +3,5 @@ from src.mysql import MySQLClient
 
 def test_mysql():
     db_server = MySQLClient()
-    todo_list = db_server.get_all_todo_list()
+    todo_list = db_server.get_all_todo_list_with_cache()
     assert len(todo_list) != 0
